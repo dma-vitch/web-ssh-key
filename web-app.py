e799f7a4d73ea9ad9a49600d6533341a580ff100
Ilya Shataev <ilya.shataev@gmail.com>
2017-04-06 20:13:07 +0300

    start web interface

--- web-app.py
+++ web-app.py
@@ -1,9 +1,15 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, render_template, url_for, redirect, jsonify, request, g, session, flash
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# Render login page
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run()
